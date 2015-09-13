910c431dd2f645be82a34f9d1163f3ee3f015abe
Toshio Ito <debug.ito@gmail.com>
2015-09-13 16:39:37 +0900

    Write StopWatchSpec.

--- test/Control/StopWatchSpec.hs
+++ test/Control/StopWatchSpec.hs
@@ -1,12 +1,27 @@
 module Control.StopWatchSpec (main, spec) where
 
+import Control.Concurrent (threadDelay)
+
 import Test.Hspec
+
+import Control.StopWatch (stopWatch)
 
 main :: IO ()
 main = hspec spec
 
+testStopWatch :: Int -> Int -> Expectation
+testStopWatch threshold_range_msec target_msec = do
+  (_, difftime) <- stopWatch $ threadDelay (target_msec * 1000)
+  let got_diff_msec = floor (difftime * 1000)
+  got_diff_msec `shouldSatisfy` (>= target_msec - threshold_range_msec)
+  got_diff_msec `shouldSatisfy` (<= target_msec + threshold_range_msec)
+
+specStopWatch :: Int -> Int -> Spec
+specStopWatch trange target = it ("should work for target " ++ (show target) ++ "msec and range " ++ (show trange) ++ "msec") $
+  testStopWatch trange target
+
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
-      True `shouldBe` False
+  describe "stopWatch" $ do
+    let trange = 30
+    mapM_  (specStopWatch trange) [50, 100, 500, 1000]
