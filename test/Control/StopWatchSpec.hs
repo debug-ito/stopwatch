f2af3a48d09b763a24ce2b53cfa9cdb0bd712825
Toshio Ito <debug.ito@gmail.com>
2016-05-03 16:41:59 +0900

    StopWatchSpec: use 'shouldSatisfy' directly on 'TimeSpec'
    for better error message on failure.

--- test/Control/StopWatchSpec.hs
+++ test/Control/StopWatchSpec.hs
@@ -6,30 +6,28 @@ import Control.Concurrent (threadDelay)
 import Test.Hspec
 
 import Control.StopWatch (stopWatch)
-import System.Clock (sec, nsec)
+import System.Clock (TimeSpec(sec, nsec))
 
 main :: IO ()
 main = hspec spec
 
-checker :: Int -> Int -> (String, Int -> Expectation)
+checker :: Int -> Int -> (String, TimeSpec -> Bool)
 checker threshold_range_msec target_msec = (description, checker_func) where
+  ts2msec ts = fromIntegral $ (sec ts) * 1000 + ((nsec ts) `div` 1000000)
 #ifdef TEST_DELAY_UPPER_BOUND
   description = "target " ++ (show target_msec) ++ "msec and range +- " ++ (show threshold_range_msec) ++ "msec"
-  checker_func got_diff_msec = do
-    got_diff_msec `shouldSatisfy` (>= target_msec - threshold_range_msec)
-    got_diff_msec `shouldSatisfy` (<= target_msec + threshold_range_msec)
+  checker_func got = (ts2msec got >= target_msec - threshold_range_msec)
+                     && (ts2msec got <= target_msec + threshold_range_msec)
 #else
   description = "target " ++ (show target_msec) ++ "msec and lower bound -" ++ (show threshold_range_msec) ++ "msec"
-  checker_func got_diff_msec = do
-    got_diff_msec `shouldSatisfy` (>= target_msec - threshold_range_msec)
+  checker_func got = (ts2msec got >= target_msec - threshold_range_msec)
 #endif
 
 specStopWatch :: Int -> Int -> Spec
 specStopWatch threshold_range_msec target_msec =
   it ("should work for " ++ description) $ do
     (_, difftime) <- stopWatch $ threadDelay (target_msec * 1000)
-    let got_diff_msec = fromIntegral $ (sec difftime) * 1000 + ((nsec difftime) `div` 1000000)
-    checkerFunc got_diff_msec
+    difftime `shouldSatisfy` checkerFunc
   where
     (description, checkerFunc) = checker threshold_range_msec target_msec
     
